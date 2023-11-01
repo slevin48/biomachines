febeb703b2dfaffb2cbdb5cbc52fe620a5ef9014
Slevin48 <yann.debray@gadz.org>
2023-10-31 21:05:05 -0400

    Order chapters and park images to the sidebar

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,17 +18,23 @@ session = boto3.Session(
 s3 = session.client('s3')
 
 def list_folders_and_files(bucket):
-    """List folders and files in a specified S3 bucket."""
-    folders = set()
+    """List top-level folders and files in a specified S3 bucket."""
+    top_level_folders = set()
     files = []
-    resp = s3.list_objects_v2(Bucket=bucket)
-    for obj in resp['Contents']:
-        key = obj['Key']
-        if '/' in key:
-            folder = key.split('/')[0]
-            folders.add(folder)
-        files.append(key)
-    return sorted(folders), files
+    paginator = s3.get_paginator('list_objects_v2')
+    pages = paginator.paginate(Bucket=bucket)
+
+    for page in pages:
+        for obj in page.get('Contents', []):
+            key = obj['Key']
+            # Check if there is a '/' in the key indicating a folder structure
+            if '/' in key:
+                folder = key.split('/')[0]
+                top_level_folders.add(folder)
+            files.append(key)
+
+    return sorted(top_level_folders), files
+
 
 def display_image(bucket, key):
     """Display image from S3 bucket."""
@@ -46,12 +52,23 @@ def download_and_display_video(bucket, key):
 st.title('Biomachines 🧬🤖')
 
 folders, all_files = list_folders_and_files(BUCKET_NAME)
-
+chapters = [
+  "0_Premonition",
+  "1_Domination",
+  "2_Conversation",
+  "3_Presentation",
+  "4_Session",
+  "5_Connection",
+  "6_Exploration",
+  "7_Rationalization",
+  "8_Union",
+  "9_Revelation",
+  "10_Manipulation",
+]
 # Sidebar for folder selection
-selected_folder = st.sidebar.selectbox("Select a chapter:", [""] + list(folders))
+selected_folder = st.selectbox("Select a chapter:", [""] + chapters)
 
 if selected_folder:
-    st.write(f'Chapter: {selected_folder}')
     video_key = f"{selected_folder}/{selected_folder}_9x16.mp4"
     if video_key in all_files:
         if st.button(f"Show Video"):
@@ -59,11 +76,13 @@ if selected_folder:
     # for file_key in all_files:
     #     if file_key.startswith(selected_folder):
     #         st.write(file_key)
-    for file_key in all_files:
-        if file_key.startswith(selected_folder+'/images'):
-            # st.sidebar.write(file_key.replace(selected_folder+'/images/',''))
-            if file_key.lower().endswith(('.png', '.jpg', '.jpeg')):
-                if st.sidebar.button(f"Show {file_key.replace(selected_folder+'/images/','')}"):
-                    display_image(BUCKET_NAME, file_key)
+    if st.checkbox('Show images'):
+        st.sidebar.subheader(f'Images: {selected_folder}')
+        for file_key in all_files:
+            if file_key.startswith(selected_folder+'/images'):
+                # st.sidebar.write(file_key.replace(selected_folder+'/images/',''))
+                if file_key.lower().endswith(('.png', '.jpg', '.jpeg')):
+                    if st.sidebar.button(file_key.replace(selected_folder+'/images/','')):
+                        display_image(BUCKET_NAME, file_key)
 else:
     st.write("Select a chapter to view its contents.")
